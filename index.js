29c7683a0b1734828acebf0cc5896e67da535b53
lislis <mail@lislis.de>
2017-11-18 15:40:16 +0100

    server serve

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var watsonUser = process.env.IBM_CLOUD_USER;
 var watsonPw = process.env.IBM_CLOUD_PW;
 var watsonVersion = process.env.IBM_CLOUD_VERSION || '2017-11-18';
 
+var port = process.env.PORT || '3002'
+
 var express = require('express');
 var app = express();
 
@@ -18,26 +20,52 @@ var personality_insights = new PersonalityInsightsV3({
 });
 
 
-var params = {
-  // Get the content items from the JSON file.
-  content_items: require('./profiles/merkel.json').contentItems,
-  consumption_preferences: true,
-  raw_scores: true,
-  headers: {
-    'accept-language': 'en',
-    'accept': 'application/json'
-  }
+function queryWatson(profileName) {
+  // From the IBM Watson Node docs
+  // promisified
+  return new Promise(function(resolve, reject) {
+    let params = {
+      content_items: require(`./profiles/${profileName}.json`).contentItems,
+      consumption_preferences: true,
+      raw_scores: true,
+      headers: {
+        'accept-language': 'en',
+        'accept': 'application/json'
+      }
+    };
+
+    personality_insights.profile(params, function(error, response) {
+      if (error) {
+        console.log('Error while getting profile data:', error);
+        reject();
+      } else {
+        resolve(response);
+      }
+    });
+  })
 };
 
-personality_insights.profile(params, function(error, response) {
-  if (error)
-    console.log('Error:', error);
-  else
-    console.log(JSON.stringify(response, null, 2));
-  }
-);
+// These are the profiles we have ready
+var politicians = ['merkel', 'rajoy', 'macron', 'may'];
+var personalities = {};
 
-app.get('/', function (req, res) {
-  res.send('hello world')
+// Let's fill the personalities data set
+politicians.forEach((k) => {
+  personalities[k] = {};
+  queryWatson(k).then((data) => { personalities[k] = data; });
+});
+
+
+
+app.get('/api/personalities', function (req, res) {
+  //console.log(personalities);
+  res.send(JSON.stringify(personalities));
 })
 
+app.get('/', function (req, res) {
+  res.send('Hello');
+});
+
+app.listen(port, function() {
+  console.log(`hey, server running at ${port}`);
+});
